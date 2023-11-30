6457d316591641fc315cb48906a4c39e52164d60
Topy <gmsdl1994@naver.com>
2023-11-30 11:17:39 +0900

    project init
    #1

---
+++ src/test/java/monitoring/monitoring_api/MonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package monitoring.monitoring_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
