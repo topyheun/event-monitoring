d85979a1a4a0dc6baafec87af00f89e14e398257
Topy <gmsdl1994@naver.com>
2024-03-03 02:14:46 +0900

    slack webhook url 상수값 정의

---
+++ src/main/java/project/common/Const.java
@@ -0,0 +1,7 @@
+package project.common;
+
+public final class Const {
+
+    public static final String SLACK_WEBHOOK_URL_USER_DOMAIN = "https://hooks.slack.com/services/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";
+    public static final String SLACK_WEBHOOK_URL_ORDER_DOMAIN = "https://hooks.slack.com/services/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";
+}
